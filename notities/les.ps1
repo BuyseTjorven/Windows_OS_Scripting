784722ae61753f5349142719d509473109c0c0c5
BuyseTjorven <tjorven.buyse@student.howest.be>
2023-03-23 11:19:52 +0100

    bleh

---
+++ notities/les.ps1
@@ -0,0 +1,33 @@
+#ja dingen enzo
+
+
+# command | gm  # geeft alle methodes van een object(dus je kan ermee zien welke properties en methodes een object heeft)
+
+#-IncludeManagementTools # Altijd doen anders geen management na installeren vanuit script grrrr
+
+#invoke-dhcpServer4FailoverReplication dingen , doen anders moet je wachten
+
+
+#abc company aanmaken kan je perfect scripten, je moet csv files maken en daaruit dingen halen met uw script
+#zelf de map waar je de homes in zet moet je aanmaken, anders is er geen en ist kapot
+
+
+
+#dingen voor user maken enzo idk
+
+#disable inheritance and remove acces
+#set full control admin
+#Inheritance "Conatiner Inherit" "Object Inherit" #dit zetten
+#propagation "none" #dit zetten
+#accescontroltype "allow" #dit zetten
+#dan e lange regel voor het effectief te setten
+
+#dan nog de dingen setten voor autentieke gebruikers enzovoort
+
+#dan gekke foreach om alles uit csv te gebruiken voor user te maken idkkkkk
+#vergeet nie -Enabled op True te zette nwnat standaard is ie disabled
+
+#bij maken user, zelf home directory aanmaken en share aanmaken, anders is er geen en ist kapot
+
+
+#ip librabry testen zo gwn script library
